4da1f45629e7380a05d917bca57150195281e8b2
Rhishavhere <rhishh.me@gmail.com>
2025-03-22 22:20:11 +0530

    ARP & ICMP

--- scanner.ps1
+++ scanner.ps1
@@ -1,20 +1,22 @@
 Write-Host "Select an option"
 Write-Host "1. Fast Scan (ping)"
 Write-Host "2. Detailed Scan (nmap)"
-$choice = Read-Host "Enter Choice : "
+Write-Host "3. Nmap ARP"
+Write-Host "2. Nmap ICMP"
+$choice = Read-Host "Enter Choice"
 
 # network range
 $network = "192.168.1"
 $results = "scan_results.txt"
 
 #clear previous results
-if (Test-Path $results){
-  Remove-Item $results
-}
+# if (Test-Path $results){
+#   Remove-Item $results
+# }
 
 if ($choice -eq "1"){
   Write-Host "`nPerforming Fast Scan..."
-  For ($i = 1; $i -le 255; $i++){
+  For ($i = 35; $i -le 45; $i++){
     $ip = "$network.$i"
     if (Test-Connection -ComputerName $ip -Count 1 -Quiet) {
       Write-Host "Host $ip is up" | Tee-Object -FilePath $results -Append
@@ -33,7 +35,43 @@ elseif ($choice -eq "2") {
 
   # Detailed nmap scan
   $fullRange = "$network.0/24"
-  nmap -sn $fullRange | Out-File -FilePath $results
+  nmap -sn -v --reason $fullRange | Tee-Object -FilePath $results
+}
+elseif ($choice -eq "3") {
+  Write-Host "`nPerforming detailed scan..."
+
+  if (-Not (Get-Command nmap -ErrorAction SilentlyContinue)) {
+    Write-Host "Nmap not found. Please install nmap and try again."
+    Exit
+  }
+
+  # Detailed nmap scan
+  $fullRange = "$network.0/24"
+  nmap -sn -PR $fullRange | Tee-Object -FilePath $results
+}
+elseif ($choice -eq "4") {
+  Write-Host "`nPerforming detailed scan..."
+
+  if (-Not (Get-Command nmap -ErrorAction SilentlyContinue)) {
+    Write-Host "Nmap not found. Please install nmap and try again."
+    Exit
+  }
+
+  # Detailed nmap scan
+  $fullRange = "$network.0/24"
+  nmap -sn -PE $fullRange | Tee-Object -FilePath $results
+}
+elseif ($choice -eq "2") {
+  Write-Host "`nPerforming detailed scan..."
+
+  if (-Not (Get-Command nmap -ErrorAction SilentlyContinue)) {
+    Write-Host "Nmap not found. Please install nmap and try again."
+    Exit
+  }
+
+  # Detailed nmap scan
+  $fullRange = "$network.0/24"
+  nmap -sn -v --reason $fullRange | Tee-Object -FilePath $results
 }
 else {
   Write-Host "Invalid choice!"
